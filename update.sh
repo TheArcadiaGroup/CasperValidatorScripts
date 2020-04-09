138f3630e9cfe92eed25f7034bc16a6ab9de45d7
Rasikh Morani <rasikh@arcadiamgroup.com>
2020-04-08 22:37:40 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+pm2 stop all
+sudo apt-get update -y
+sudo apt-get upgrade -y
+rm ~/.casperlabs/sqlite.db
+rm -r ~/.casperlabs/global_state
+rm ~/.casperlabs/chainspec/genesis/*
+cd ~/.casperlabs/chainspec/genesis
+curl -O https://raw.githubusercontent.com/CasperLabs/CasperLabs/dev/testnet/accounts.csv
+curl -O https://raw.githubusercontent.com/CasperLabs/CasperLabs/dev/testnet/manifest.toml
+pm2 start all
